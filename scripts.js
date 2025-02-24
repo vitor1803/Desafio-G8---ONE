90985d0d0ace5819bba48279a3d6ab7dd3e2839d
Vitor Souza <vssouza.eng@gmail.com>
2025-02-24 12:44:32 -0400

    Inital version of the Challenge

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let lista_amigos = [];
+const amigos = document.getElementById('amigos');
+
+function alterar() {
+    let amigo_novo = document.getElementById('amigo_novo').value;
+    if (amigo_novo == "") {
+        alert("The Name is empty");
+    } else {
+        lista_amigos.push(amigo_novo);
+        let amigos = document.getElementById('amigos')
+        amigos.innerHTML = lista_amigos
+    }
+}
+function sortear() {
+    let indice = Math.floor(Math.random() * lista_amigos.length);
+    alert(lista_amigos[indice]);
+}
